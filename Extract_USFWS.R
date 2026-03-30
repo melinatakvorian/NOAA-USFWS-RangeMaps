fb5b8efe5c67a530d8d700ced941c38a37369542
Melina Takvorian <takvorian.melina@gmail.com>
2026-03-30 15:41:41 -0600

    extracting ALL matches instead of just MATCHES_full_scinames.csv

--- Extract_USFWS.R
+++ Extract_USFWS.R
@@ -26,165 +26,165 @@ invisible(lapply(packages, library, character.only = TRUE))
 #Read in files ----
 usfws_1 <- st_read("USFWS_rawdata/usfws_complete_species_current_range_1.shp")
 usfws_2 <- st_read("USFWS_rawdata/usfws_complete_species_current_range_2.shp")
-matches_complete <- read_csv("Output/MATCHES_full_scinames.csv")
+matches_complete <- read_csv("Output/USFWS_matches.csv")
 cemml_raw <- read_xlsx("N:/RStor/CEMML/ClimateChange/0_Natural Resources Teams/Wildlife/_Excel Files For Viewer/Species Assessments - Viewer.xlsx")
 
 #PULL INDIVIDUAL SPECIES Data ----
 ##usfws 1 ----
-MGL1_A <- matrix(nrow = 1, ncol = 11)
-colnames(MGL1_A) <-  colnames(usfws_1)
-MGL1_A <- as.data.frame(MGL1_A)
-
-num <- 0
-for(i in 1:nrow(usfws_1)){
+  MGL1_A <- matrix(nrow = 1, ncol = 11)
+  colnames(MGL1_A) <-  colnames(usfws_1)
+  MGL1_A <- as.data.frame(MGL1_A)
   
-  newdf <- usfws_1[i,]
-  sci_name <- newdf$SCINAME
-  
-  if(!sci_name %in% matches_complete$USFWS) next  #[CHECK IF NAME IS NOT IN THE MATCHES DATASET]
-  num <- num+1
-  
-  com_name <- newdf$COMNAME
-  com_name <- gsub(" ", "", tools::toTitleCase(com_name))
-  
-  speciesID <- which(cemml_raw$`Species Latin Name` == sci_name)
-  speciesID <- as.numeric(speciesID)
-  
-  print(paste(com_name, "speciesID: ",speciesID, " | has been added"))
-  
-  MGL1_A[num, ] <- usfws_1[i,]
-}
+  num <- 0
+  for(i in 1:nrow(usfws_1)){
+    
+    newdf <- usfws_1[i,]
+    sci_name <- newdf$SCINAME
+    
+    if((!sci_name %in% matches_complete$Species.Latin.Name) && (!sci_name %in% matches_complete$short_name)) next  #[CHECK IF NAME IS NOT IN THE MATCHES DATASET]
+    num <- num+1
+    
+    com_name <- newdf$COMNAME
+    com_name <- gsub(" ", "", tools::toTitleCase(com_name))
+    
+    speciesID <- which(cemml_raw$`Species Latin Name` == sci_name)
+    speciesID <- as.numeric(speciesID)
+    
+    print(paste(com_name, "speciesID: ",speciesID, " | has been added"))
+    
+    MGL1_A[num, ] <- usfws_1[i,]
+  }
 
 ##usfws 2 ----
-MGL2_A <- matrix(nrow = 1, ncol = 11)
-colnames(MGL2_A) <-  colnames(usfws_1)
-MGL2_A <- as.data.frame(MGL2_A)
-
-num <- 0
-for(i in 1:nrow(usfws_2)){
+  MGL2_A <- matrix(nrow = 1, ncol = 5)
+  colnames(MGL2_A) <-  colnames(usfws_2)
+  MGL2_A <- as.data.frame(MGL2_A)
   
-  newdf <- usfws_2[i,]
-  sci_name <- newdf$SCINAME
+  num <- 0
+  for(i in 1:nrow(usfws_2)){
+    
+    newdf <- usfws_2[i,]
+    sci_name <- newdf$SCINAME
+    
+    if((!sci_name %in% matches_complete$Species.Latin.Name) && (!sci_name %in% matches_complete$short_name)) next  #[CHECK IF NAME IS NOT IN THE MATCHES DATASET]
+    num <- num+1
+    
+    com_name <- newdf$COMNAME
+    com_name <- gsub(" ", "", tools::toTitleCase(com_name))
+    
+    speciesID <- which(cemml_raw$`Species Latin Name` == sci_name)
+    speciesID <- as.numeric(speciesID)
+    
+    print(paste(com_name, "speciesID: ",speciesID, " | has been added"))
+    
+    MGL2_A[num, ] <- usfws_2[i,]
+  }
   
-  if(!sci_name %in% matches_complete$USFWS) next  #[CHECK IF NAME IS NOT IN THE MATCHES DATASET]
-  num <- num+1
-  
-  com_name <- newdf$COMNAME
-  com_name <- gsub(" ", "", tools::toTitleCase(com_name))
-  
-  speciesID <- which(cemml_raw$`Species Latin Name` == sci_name)
-  speciesID <- as.numeric(speciesID)
-  
-  print(paste(com_name, "speciesID: ",speciesID, " | has been added"))
-  
-  MGL2_A[num, ] <- usfws_2[i,]
-}
-
-MGL_B <- bind_rows(MGL1_A, MGL2_A)
+  MGL_B <- bind_rows(MGL1_A, MGL2_A)
 
 #MAKE GROUP OF SEPERATES ----
-MGL_B_mult <- MGL_B  %>%
-  group_by(COMNAME) %>%
-  filter(n() > 1) %>%
-  ungroup() 
-
-MGL_B_mult <- st_sf(MGL_B_mult)
+  MGL_B_mult <- MGL_B  %>%
+    group_by(COMNAME) %>%
+    filter(n() > 1) %>%
+    ungroup() 
+  
+  MGL_B_mult <- st_sf(MGL_B_mult)
 
 
 #create list of multiples similar ----
 
-MGL_singles <- MGL_B_mult[0,] #where the final, merged data will go into
-
-# Get unique species names
-species_list <- unique(MGL_B_mult$SCINAME)
-
-for (species in species_list) {
-  # Get all rows for this species
-  species_rows <- MGL_B_mult[MGL_B_mult$SCINAME == species, ]
+  MGL_singles <- MGL_B_mult[0,] #where the final, merged data will go into
   
-  # Merge geometries
-  merged_geom <- st_union(species_rows)
+  # Get unique species names
+  species_list <- unique(MGL_B_mult$SCINAME)
   
-  # Take attributes from first row
-  merged_row <- species_rows[1, ]
-  merged_row$geometry <- merged_geom
+  for (species in species_list) {
+    # Get all rows for this species
+    species_rows <- MGL_B_mult[MGL_B_mult$SCINAME == species, ]
+    
+    # Merge geometries
+    merged_geom <- st_union(species_rows)
+    
+    # Take attributes from first row
+    merged_row <- species_rows[1, ]
+    merged_row$geometry <- merged_geom
+    
+    # Append to final sf
+    MGL_singles <- rbind(MGL_singles, merged_row)
+  }
   
-  # Append to final sf
-  MGL_singles <- rbind(MGL_singles, merged_row)
-}
+  #delete the occurrences of species from species_list ----
+  MGL_C <- MGL_B %>% 
+    filter(!SCINAME %in% species_list)
+  
+  #add in final species files ----
+  MGL_D <- rbind(MGL_C, MGL_singles)
+  
+  #set coordinate reference system to be the same as the original data`
+  #transform to spatial dataset
+  MGL_D <- st_sf(MGL_D)
 
-#delete the occurrences of species from species_list ----
-MGL_C <- MGL_B %>% 
-  filter(!SCINAME %in% species_list)
-
-#add in final species files ----
-MGL_D <- rbind(MGL_C, MGL_singles)
-
-#set coordinate reference system to be the same as the original data`
-#transform to spatial dataset
-MGL_D <- st_sf(MGL_D)
-
-#add CRS`
-st_crs(MGL_D) <- st_crs(usfws_1)
-
-##add speciesID
-for(i in 1:nrow(MGL_D)){
+  #add CRS`
+  st_crs(MGL_D) <- st_crs(usfws_1)
   
-  sci_name <- MGL_D$SCINAME[i] #get name for ID
-  
-  speciesID <- which(cemml_raw$`Species Latin Name` == sci_name) #find speciesID from list
-  speciesID <- as.numeric(speciesID) #convert the ID to a number
-  
-  MGL_D$speciesID[i] <- speciesID #assign this ID to a new column
-  
-  print(paste(sci_name, "speciesID: ", speciesID, " | has been added"))
-}
+  ##add speciesID
+  for(i in 1:nrow(MGL_D)){
+    
+    sci_name <- MGL_D$SCINAME[i] #get name for ID
+    
+    speciesID <- which(cemml_raw$`Species Latin Name` == sci_name) #find speciesID from list
+    speciesID <- as.numeric(speciesID) #convert the ID to a number
+    
+    MGL_D$speciesID[i] <- cemml_raw$`Species ID#`[speciesID] #assign this ID to a new column
+    
+    print(paste(sci_name, "speciesID: ", speciesID, " | has been added"))
+  } #errors but not sure why, because the table looks good
 
 #EXPORTING ----
 
 #EXPORT ALL FILES IN LIST THAT HAVE NOT BEEN DONE ALREADY
 
 #store folder path
-shapefile_folder <- "N:/RStor/CEMML/ClimateChange/0_Natural Resources Team/Wildlife/_RangeMaps/Shapefiles"
+  shapefile_folder <- "N:/RStor/CEMML/ClimateChange/0_Natural Resources Team/Wildlife/_RangeMaps/Shapefiles"
 
-done_files <- "N:/RStor/CEMML/ClimateChange/0_Natural Resources Teams/Wildlife/_RangeMaps/Shapefiles/Temporary"
+  done_files <- "N:/RStor/CEMML/ClimateChange/0_Natural Resources Teams/Wildlife/_RangeMaps/Shapefiles/Temporary"
 
 #identify files that are already completed
-speciesdone <- list.files(path = done_files, pattern = "\\.shp$")
+  speciesdone <- list.files(path = done_files, pattern = "\\.shp$")
 
 #remove .shp ending to be able to run comparison later
-for(i in 1:length(speciesdone)){
-  strL <- str_length(speciesdone[i])
-  newL <- strL - 4
-  speciesdone[i] <- str_sub(speciesdone[i],1,newL)
-}
-
-
-for(i in 1:nrow(MGL_D)){
-  
-  #pull list item out so that it can be saved individually
-  species_pull <- MGL_D[i,]
-  
-  ##pull species common name ----
-  name <- species_pull$COMNAME
-  
-  if(name == "No common name"){
-    name <- species_pull$SCINAME
+  for(i in 1:length(speciesdone)){
+    strL <- str_length(speciesdone[i])
+    newL <- strL - 4
+    speciesdone[i] <- str_sub(speciesdone[i],1,newL)
   }
-  
-  name <- gsub(" ", "", tools::toTitleCase(name))
-  name <- gsub("'", "", name)
-  
-  #CHECK TO SEE IF ALREADY COMPLETED -> IF NOT, create unique name for file
-  if(name %in% speciesdone) next #SKIP THIS SPECIES BECAUSE IT IS ALREADY DONE and in Temporary folder
-  
-  #create name using name
-  shapefile_location <- paste0(shapefile_folder,"/", name, ".shp")
-  
-  #export shapefile to file path
-  st_write(species_pull, shapefile_location, append=FALSE)
-  
-}
+
+#checking existing files and exporting necessary ones
+  for(i in 1:nrow(MGL_D)){
+    
+    #pull list item out so that it can be saved individually
+    species_pull <- MGL_D[i,]
+    
+    ##pull species common name ----
+    name <- species_pull$COMNAME
+    
+    if(name == "No common name"){
+      name <- species_pull$SCINAME
+    }
+    
+    name <- gsub(" ", "", tools::toTitleCase(name))
+    name <- gsub("'", "", name)
+    
+    #CHECK TO SEE IF ALREADY IN FOLDER (if yes, do not write into folder) -> IF NOT, create unique name for file
+    if(name %in% speciesdone) next 
+    
+    #create name using name
+    shapefile_location <- paste0(done_files,"/", name, ".shp")
+    
+    #export shapefile to file path
+    st_write(species_pull, shapefile_location, append=FALSE)
+    
+  }
 
 #make csv for logging in the table for Adam
 export_csv <- MGL_D[ ,c(6,7,12)] #only keep common name, scientific name, speciesID
